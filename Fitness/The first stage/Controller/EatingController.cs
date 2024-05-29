e4a4b84f581780a2505d1aec2c5a3d78639392bc
WIN-FI93QU5E7B9\Asidletska <nastiasidletskaya@gmail.com>
2024-05-29 11:47:25 +0300

    Add callories/ Proteins/ Fats / Carbohydrates

---
+++ Fitness/The first stage/Controller/EatingController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using The_first_stage.Model;
+
+namespace The_first_stage.Controller
+{
+    public class EatingController
+    {
+        private readonly User user;
+        public List<Food> Foods { get; set; }
+        public EatingController(User user)
+        {
+            this.user = user ?? throw new ArgumentNullException("Username can`t be empty", nameof(user));
+
+            Foods = GetAllFoods();
+        }
+        private List<Food> GetAllFoods()
+        {
+            var formatter = new BinaryFormatter();
+            using (var fs = new FileStream("foods.dat", FileMode.OpenOrCreate))
+            {
+                if (fs.Length > 0 && formatter.Deserialize(fs) is List<Food> foods)
+                {
+                    return foods;
+                }
+                else
+                {
+                    return new List<Food>();
+                }
+            }
+        }
+        
+    }
+}
---
+++ Fitness/The first stage/Model/Food.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace The_first_stage.Model
+{
+    public class Food
+    {
+        public string Name { get; }
+        /// <summary>
+        /// Белки
+        /// </summary>
+        public double Proteins {get; }
+        /// <summary>
+        /// Жири
+        /// </summary>
+        public double Fats { get; }
+        /// <summary>
+        /// Углеводы
+        /// </summary>
+        public double Carbohydrates { get;}
+        /// <summary>
+        /// Калории за 100 гр продукта
+        /// </summary>
+        public double Callories { get;}
+
+        private double CalloriesOneGram { get { return Callories / 100.0; } }
+        private double ProteinOneGram { get { return Proteins / 100.0; } }
+        private double FatsOneGram { get { return Fats / 100.0; } }
+        private double CarbohydratesOneGram { get { return Carbohydrates / 100.0; } }
+
+        public Food(string name) : this (name, 0, 0, 0, 0) { }
+        public Food(string name, double callories, double proteins, double fats, double carbohydrates)
+        {
+            // TODO: check
+            Name = name;
+            Callories = callories / 100.0;
+            Proteins = proteins / 100.0;
+            Fats = fats / 100.0;
+            Carbohydrates = carbohydrates / 100.0;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
